74752a3e2827f87b7fd50c35d6e7db4e8e1aa3c6
andre.kortkamp <andre.kortkamp@genezys.io>
2023-07-24 10:28:03 -0300

    hardhat config testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,22 @@ module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
-  }
+  },
+ networks: {
+    hardhat: {
+    	localhost: {
+    		url: "http://127.0.0.1:8545/",
+    	},
+     // forking: {
+     //   url: process.env.GOERLI_RPC_URL,
+     //   accounts: [process.env.PRIVATE_KEY],
+     // },
+    },
+  },
+  settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
 };
